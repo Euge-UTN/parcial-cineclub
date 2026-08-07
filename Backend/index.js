2ed27de709ade163c0bdec51f20036f2a21d996d
Eugenia <eugeniafer2005@gmail.com>
2026-08-07 17:20:04 -0300

    Configurar backend inicial

---
+++ Backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutándose en el puerto ${PORT}`)
+})
