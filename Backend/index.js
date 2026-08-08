f68db3195bc7ce5bebf62d7882776b4f763ae820
Eugenia <eugeniafer2005@gmail.com>
2026-08-08 15:36:23 -0300

    Búsqueda de películas en TMDB

--- Backend/index.js
+++ Backend/index.js
@@ -14,6 +14,28 @@ app.get('/api/test', (request, response) => {
   response.json({ message: 'Backend funcionando correctamente' })
 })
 
+app.get('/api/movies/search', async (request, response) => {
+  const query = request.query.q
+
+  if (!query) {
+  return response.status(400).json({
+    error: 'El parámetro q es obligatorio'
+  })
+}
+
+  const url = `https://api.themoviedb.org/3/search/movie?query=${query}`
+
+  const result = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${process.env.TMDB_API_KEY}`
+    }
+  })
+
+  const data = await result.json()
+
+  response.json(data)
+})
+
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en el puerto ${PORT}`)
 })
